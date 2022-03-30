89ea6d1f265bb02de4bad585a14e541b50a9cdb2
Microsoft <matthewpianst24241717@gmail.com>
2022-03-30 15:56:32 +0800

    added firey ingot

--- src/main/java/net/littlepianist/crashingbosses/CrashingBosses.java
+++ src/main/java/net/littlepianist/crashingbosses/CrashingBosses.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,8 +27,11 @@ public class CrashingBosses {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public CrashingBosses() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
